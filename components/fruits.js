84e01ee5bea538e33bae5f51bdffa3aff3bab5f7
marcnjaramillo <marcn.jaramillo@gmail.com>
2017-04-28 21:41:17 -0700

    Done.

---
+++ components/fruits.js
@@ -0,0 +1,5 @@
+export default {
+  apple: 'red',
+  banana: 'yellow',
+};
+ 
--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const app = "I don't do much.";
+import {message} from './components/foo'
+
+// In a file in the same directory
+import fruit from './components/fruits'
+console.log(fruits.apple); // prints 'red'
+
+// In another file, also in the same directory
+import {apple} from './components/fruits'
+console.log(apple); // prints 'red'
+
+import Tweet from './components/Tweet'
+import ReactDOM from 'react-dom'
+ReactDOM.render(
+  <Tweet />,
+  document.getElementById('main')
+);
